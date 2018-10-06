0fa4558602c7407b082e8c60e622f178ea84f1cf
GMT95 <gmtalha9@gmail.com>
2018-10-07 00:20:11 +0500

    1st commit fbpost like something

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Post from './components/post';
+
+const PropsObject = {
+  createdBy: 'Talha',
+  avatar: 'T',
+  images: ['https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/SDAR.doi.320.high.imageLargeThreeQuarterNodePath.png/1522248411370.png',
+          'https://media.performancebike.com/images/performance/products/product-hi/31-6715-BLK-ANGLE.jpg?resize=415px:415px&output-quality=85',
+          'http://rnbquarterhorses.com/wp-content/uploads/sites/34/2017/09/55b87914990116205b4aa75205f900d2-american-quarter-horse-horse-breeds-300x294.jpg'        
+  ],
+  description: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
+  createdAt: new Date().toDateString(),
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>      
+        <Post data={PropsObject}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/post.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FbImageGrid from 'react-fb-image-grid';
+import '../App.css'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class Post extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+    const {data} = this.props
+    return (
+      
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              {data.avatar}
+            </Avatar>
+          }
+          title={data.createdBy}
+          subheader={data.createdAt}
+        />
+        {/* put sir kashif library here */}
+        <CardContent>
+          <div className="grid">
+          <FbImageGrid 
+              images={data.images} 
+              countFrom={5}
+              width={100}
+              align="left"
+          />
+          </div>
+          <Typography component="p">
+            {this.props.description}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+
+      </Card>
+    );
+  }
+}
+
+Post.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Post);
